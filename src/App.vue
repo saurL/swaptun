cc9c90a60bb28a39290af791ac3771a77ade0d93
Lucas Sauron <sauronlucas@gmail.com>
2025-04-02 17:05:35 +0200

    went from nuxt to vue

--- src/App.vue
+++ src/App.vue
@@ -15,17 +15,6 @@ async function greet() {
   <main class="container">
     <h1>Welcome to Tauri + Vue</h1>
 
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
     <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
 
     <form class="row" @submit.prevent="greet">
@@ -44,7 +33,6 @@ async function greet() {
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #249b73);
 }
-
 </style>
 <style>
 :root {
@@ -156,5 +144,4 @@ button {
     background-color: #0f0f0f69;
   }
 }
-
 </style>
