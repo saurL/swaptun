0e4d839dcfa8de9ed5a2e9a06a700065796a8353
Lucas Sauron <sauronlucas@gmail.com>
2025-04-26 21:10:52 +0200

    changed background location

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <router-view />
+  <div
+    class="min-h-screen w-screen bg-[#121212] text-white flex flex-col pb-20"
+  >
+    <router-view />
+  </div>
 </template>
